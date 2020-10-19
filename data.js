a3de6936bc2459fa3f9168bfad49e3e335fe0fad
KilianDesign <73109012+KilianDesign@users.noreply.github.com>
2020-10-19 12:41:42 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,208 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-padilion6",
+      "name": "padilion.6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -2.5207093575719135,
+        "pitch": 0.8972335794440554,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2947532784445457,
+          "pitch": 0.8916956533347093,
+          "rotation": 0,
+          "target": "1-padilion7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-padilion7",
+      "name": "padilion.7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -3.0661331345819036,
+        "pitch": 0.17578919894553557,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0991862751618164,
+          "pitch": 0.20218644600065616,
+          "rotation": 0,
+          "target": "2-padilion8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-padilion8",
+      "name": "padilion.8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -2.765759046144094,
+        "pitch": 0.04568295229830355,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.722802392395156,
+          "pitch": 0.007250609912698991,
+          "rotation": 0,
+          "target": "3-padilion9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-padilion9",
+      "name": "padilion.9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 2.6850259360953412,
+        "pitch": 0.07766090924501512,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3244181071043357,
+          "pitch": 0.17650791951599132,
+          "rotation": 0,
+          "target": "4-padilion13"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-padilion13",
+      "name": "padilion.13",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -2.9243597795786087,
+        "pitch": 0.10012579265206156,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.897240269354497,
+          "pitch": 0.14086610665146537,
+          "rotation": 0,
+          "target": "5-padilion12"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-padilion12",
+      "name": "padilion.12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": -2.817812921432676,
+        "pitch": 0.3844162753835203,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
